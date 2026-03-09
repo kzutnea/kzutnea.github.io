024d01a3fd059acb70fe85471b0f5f8f0d4560a6
Vantevia <49812644+vantevia@users.noreply.github.com>
2026-03-09 15:04:55 -0500

    hi

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@ import routes from './routes.js';
 
 export const store = Vue.reactive({
     dark: localStorage.getItem('dark') === null ? true : JSON.parse(localStorage.getItem('dark')),
+    mobile: localStorage.getItem('mobile') === 'true',
     toggleDark() {
         this.dark = !this.dark;
         localStorage.setItem('dark', JSON.stringify(this.dark));
+    },
+    toggleMobile() {
+        this.mobile = !this.mobile;
+        localStorage.setItem('mobile', this.mobile);
     },
 });
 
